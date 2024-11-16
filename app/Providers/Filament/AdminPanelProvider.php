d7d1568dc520bc5be119226ad154c6ef4f51d818
Esraa Al Ghabra <esraaalghabra3040@gmail.com>
2024-11-16 11:32:09 +0300

    add logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,7 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Tajawal')
             ->brandLogo(asset(''))
-            ->favicon(asset('BRAND/LOGO.png'))
+            ->favicon(asset('BRAND/logo.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
